e0d776db00b8a627c19596bf937249fb66a1d0f1
jcarpio <jose.carpio@gmail.com>
2025-03-23 11:28:29 +0100

    Update max-width-wrapper.tsx

--- src/components/shared/max-width-wrapper.tsx
+++ src/components/shared/max-width-wrapper.tsx
@@ -1,11 +1,10 @@
 import { ReactNode } from "react";
-
 import { cn } from "@/lib/utils";
 
 export default function MaxWidthWrapper({
   className,
-  children,
   large = false,
+  children,
 }: {
   className?: string;
   large?: boolean;
@@ -14,9 +13,13 @@ export default function MaxWidthWrapper({
   return (
     <div
       className={cn(
-        "container",
-        large ? "max-w-screen-2xl" : "max-w-6xl",
-        className,
+        // Ocupa toda la pantalla en móviles (w-full, sin padding)
+        // y aplica márgenes solo en pantallas medianas hacia arriba (md:)
+        "w-full px-0 md:px-4",
+        large
+          ? "md:max-w-screen-2xl md:mx-auto"
+          : "md:max-w-6xl md:mx-auto",
+        className
       )}
     >
       {children}
